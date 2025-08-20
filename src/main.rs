0a238a5608dc043640c365826665e3e9c30aaeee
Fernando Levin <flevin58@gmail.com>
2025-08-20 23:14:12 +0200

    First release

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+// Practice url: https://jsonplaceholder.typicode.com/users
+
+use reqwest::{self, Method};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Geo {
+    lat: String,
+    lng: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Address {
+    street: String,
+    suite: String,
+    city: String,
+    zipcode: String,
+    geo: Geo,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Company {
+    name: String,
+    #[serde(rename = "catchPhrase")]
+    catchphrase: String,
+    bs: String,
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct User {
+    id: usize,
+    name: String,
+    username: String,
+    email: String,
+    address: Address,
+    phone: String,
+    website: String,
+    company: Company,
+}
+
+fn main() {
+    const URL: &str = "https://jsonplaceholder.typicode.com/users";
+    let client = reqwest::blocking::Client::new();
+    let response = client
+        .request(Method::GET, URL)
+        .header(
+            "User-Agent",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 15_6) Rust-powered",
+        )
+        .send();
+    let body = match response {
+        Ok(resp) => resp.text().unwrap(),
+        Err(_) => {
+            eprintln!("Error getting json data from server");
+            return;
+        }
+    };
+    let users: Vec<User> = match serde_json::from_str(&body) {
+        Ok(u) => u,
+        Err(_) => {
+            eprintln!("Error parsing json!");
+            return;
+        }
+    };
+
+    // Display result
+    println!("Found {} users:", users.len());
+    for (i, user) in users.iter().enumerate() {
+        println!("{:>2}: {} <{}>", i + 1, user.name, user.email);
+    }
+}
